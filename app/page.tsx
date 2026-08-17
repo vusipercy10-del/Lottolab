da06411cac6d4dbb59fd730e717ea4bafab7dd42
vusipercy10-del <vusipercy10@gmail.com>
2026-08-17 07:58:00 +0200

    Integrate Wheel/Bo​​oms/Sections/Partners fusion into LottoLab home

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,13 @@
-export { default } from '../page';
+'use client';
+
+import Dashboard from '../page';
+import StructuralFusion from '../components/structural-fusion';
+
+export default function Home() {
+  return (
+    <>
+      <Dashboard />
+      <StructuralFusion />
+    </>
+  );
+}
